1341199d530c404a4d38c1718f05429d4fe3ee7b
ERIK ALVES BRIGIDO DOS SANTOS <101717505+ErikBrigido@users.noreply.github.com>
2023-09-23 17:19:55 -0300

    Add files via upload
    Aula 2 de Python no Curso introdutório do Senai.

---
+++ AULA_2_PYTHON.py
@@ -0,0 +1,25 @@
+#--------- 1 - CRIE UM SISTEMA QUE SOME 2  NUMEROS--------
+
+a = int(input("Digite aqui o primeiro numero :"))
+b = int(input("Digite aqui o segundo numero :"))
+c = a + b
+print(c)
+
+#---------2 - CRIE UM SISTEMA QUE IMPRIMA SEU NOME NA TELA  -------
+
+nome = input("Digite aqui o seu nome :")
+print (nome)
+
+# -------3 - CRIE UM SISTEMA QUE IMPRIMA SEU NOME E UM CUMPRIMENTO APÓS nome(CONCATENE) ---
+
+nome = input("Digite aqui o seu nome :")
+cumprimento = ".Olá, tudo bem?"
+print (nome,cumprimento)
+
+#-------4 - CRIE UM SISTEMA QUE MULTIPLIQUE 2 NÚMEROS ------
+
+numero1 = float(input("Digite aqui o primeiro numero : "))
+numero2 = float(input("Digite aqui o segundo numero : "))
+multiplicação = numero1*numero2
+
+print(multiplicação)
